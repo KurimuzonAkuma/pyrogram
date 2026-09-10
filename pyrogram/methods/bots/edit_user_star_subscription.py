2f915c3a10bf2a0d4cc354456d77649290e202a9
KurimuzonAkuma <arturgayauskas@gmail.com>
2026-09-10 16:51:53 +0300

    fix: invert is_canceled param in edit_user_star_subscription
    Closes #423

--- pyrogram/methods/bots/edit_user_star_subscription.py
+++ pyrogram/methods/bots/edit_user_star_subscription.py
@@ -41,8 +41,8 @@ class EditUserStarSubscription:
                 Telegram payment identifier of the subscription.
 
             is_canceled (``bool``):
-                Pass True to cancel the subscription.
-                Pass False to allow the user to enable it.
+                Pass *True* to cancel extension of the user subscription, the subscription must be active up to the end of the current subscription period.
+                Pass *False* to allow the user to re-enable a subscription that was previously canceled by the bot.
 
         Returns:
             ``bool``: On success, True is returned.
@@ -51,6 +51,6 @@ class EditUserStarSubscription:
             raw.functions.payments.BotCancelStarsSubscription(
                 user_id=await self.resolve_peer(user_id),
                 charge_id=telegram_payment_charge_id,
-                restore=is_canceled,
+                restore=not is_canceled,
             )
         )
